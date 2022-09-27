3d98cbb4eebf335fd59d748de8981eef30d0bc32
HoangCode <you@example.com>
2022-09-27 16:48:28 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,24 @@
-const getName = function () {
-  const req = new XMLHttpRequest();
-  req.addEventListener("load", reqListener);
-  req.open("GET", "https://api.nationalize.io/?name=");
-  req.send();
-};
-function reqListener() {
-  console.log(this.responseText);
-}
-
 //get value
 let nameInput = "";
 const getInput = document.getElementById("getInputValue");
-
 getInput.addEventListener("click", function () {
   nameInput = document.getElementById("nameInput").value;
   console.log(nameInput);
 });
+
+const getName = function () {
+  //Tạo mới XHR
+  const req = new XMLHttpRequest();
+  //Gán sự kiện cho request
+  req.addEventListener("load", reqListener);
+  //Setup mở 1 GET request đến đường link
+  req.open("GET", `https://api.nationalize.io/?name=${nameInput}`);
+  //call RQ
+  req.send();
+};
+function reqListener() {
+  const data = JSON.parse(this.responseText);
+  console.log(data);
+}
+
+getName();
